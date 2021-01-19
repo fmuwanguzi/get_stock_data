fb9a578f2a974634d9a7c5d728bab684d95d9547
Felix Muwanguzi <felixm@Felixs-MacBook-Air.local>
2021-01-19 16:38:20 -0500

    object working

---
+++ stock_data.py
@@ -0,0 +1,8 @@
+####  imports  ####
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from bs4 import BeautifulSoup
+import datetime, re, requests, io, time, random, string
+from credentials import email, password
+print('everything good')
